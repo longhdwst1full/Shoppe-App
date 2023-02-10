453bb0548215643fd2a17a7f11c6acd6417bc455
long1703@outlook.com <long1703@outlook.com>
2023-02-10 15:01:24 +0700

    Xử lý lỗi message đơn thuần với axios interceptor

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,25 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    // Thêm một bộ đón chặn response
+    this.instance.interceptors.response.use(
+      function (response) {
+        // Bất kì mã trạng thái nào nằm trong tầm 2xx đều khiến hàm này được trigger
+        // Làm gì đó với dữ liệu response
+        return response
+      },
+      function (error: AxiosError) {
+        // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
+        // Làm gì đó với lỗi response
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
