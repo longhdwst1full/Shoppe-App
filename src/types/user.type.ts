e6db68a173db073ca819c1e8100d19fa80e6154d
long1703@outlook.com <long1703@outlook.com>
2023-03-07 15:37:27 +0700

    Khai báo và test API cho phần profile

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,12 +1,14 @@
 type Roles = 'Admin' | 'User'
+
 export interface User {
   _id: string
   roles: Roles[]
   email: string
-  name: string
-  date_of_birth: null
-  addresss: string
-  phone: string
+  name?: string
+  date_of_birth: string //ISO 8610
+  avatar?: string
+  addresss?: string
+  phone?: string
   createdAt: string
   updatedAt: string
 }
